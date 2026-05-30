7cae69574d7d3a470de36fa082c27e3b0afc470a
F3lip8 <128184907+DevFelas@users.noreply.github.com>
2026-05-30 17:15:21 -0300

    fix ALLOWED_HOSTS

--- reservaif/settings.py
+++ reservaif/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['projeto-refeitorio.onrender.com']
 
 
 # Quick-start development settings - unsuitable for production
