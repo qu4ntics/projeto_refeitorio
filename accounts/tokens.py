c68e9ef09beb8410d276b6fd1c22df009737ac34
F3lip8 <128184907+DevFelas@users.noreply.github.com>
2026-07-02 14:10:56 -0300

    chore: remove arquivos da raiz após migração para src/

--- accounts/tokens.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-
-class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return f'{user.pk}{timestamp}{user.email}{user.is_active}'
-
-
-email_verification_token = EmailVerificationTokenGenerator()
--- refeicoes/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class RefeicoesConfig(AppConfig):
-    name = 'refeicoes'
-
-    def ready(self):
-        import refeicoes.signals  # noqa: F401
